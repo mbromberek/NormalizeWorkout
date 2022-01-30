077711c9b985eaeffc60312e10bbb2eb47fee48c
Mike Bromberek <mikebromberek@gmail.com>
2022-01-30 08:16:05 -0600

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='NormalizeWorkout',
-      version='0.1',
+      version='1.0',
       description='Normalize Workout to a Pandas dataframe from .fit or rungap.json file',
       # url='http://github.com/storborg/funniest',
       author='Mike Bromberek',
